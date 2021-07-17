abf0316f20088cc2752d7e839d2fad7e49884bdb
Patrick Sullivan <patrick.sullivan@hey.com>
2021-07-17 16:22:08 -0400

    InnerSamplerState

--- src/sampler/mod.rs
+++ src/sampler/mod.rs
@@ -1,6 +1,8 @@
-use cgmath::Point2;
+mod state;
 
 use crate::camera::CameraSample;
+use cgmath::Point2;
+use core::f32;
 
 /// Defines the ability to generate multi-dimensional sample vectors for pixels.
 pub trait Sampler {
@@ -11,13 +13,14 @@ pub trait Sampler {
     /// Start sampling work on a given pixel. All subseqent requests to the
     /// sampler will generate samples for the given pixel, up until
     /// `start_pixel` is called again with a different pixel.
-    fn start_pixel(pixel: Point2<usize>);
+    fn start_pixel(&mut self, pixel: Point2<usize>);
 
-    /// Get a 1D sample for the next dimension of the sample vector. This method
-    /// mutates the sampler by incrementing the current sample dimension by one.
+    /// Get a 1D value for the next dimension of the current sample vector. This
+    /// method mutates the sampler by incrementing the current sample dimension
+    /// by one.
     fn get_1d(&mut self) -> f32;
 
-    /// Get a 2D sample for the next two dimensions of the current sample
+    /// Get a 2D value for the next two dimensions of the current sample
     /// vector. This method mutates the sampler by incrementing the current
     /// sample dimension by two.
     fn get_2d(&mut self) -> Point2<f32>;
@@ -39,49 +42,49 @@ pub trait Sampler {
         }
     }
 
-    /// Inform the sampler that we will want to request a vector of 1D samples
-    /// that has a length of `sample_count`. This tells the sampler to do the
-    /// preprocessing necessary for it to return the vector of samples in an
+    /// Inform the sampler that we will want to request a vector of 1D values of
+    /// length `count` for the current sample. This tells the sampler to do the
+    /// preprocessing necessary for it to return the vector of values in an
     /// efficient manner.
     ///
-    /// This must be called before rendering begins and before the samples are
-    /// actually requested using `get_1d_array`.
-    fn prepare_1d_array(&mut self, sample_count: usize);
+    /// This must be called before rendering begins for the current pixel and
+    /// before the samples are actually requested using `get_1d_array`.
+    fn prepare_1d_array(&mut self, count: usize);
 
-    /// Inform the sampler that we will want to request a vector of 2D samples
-    /// that has a length of `sample_count`. This tells the sampler to do the
-    /// preprocessing necessary for it to return the vector of samples in an
+    /// Inform the sampler that we will want to request a vector of 2D values of
+    /// length `count` for the current sample. This tells the sampler to do the
+    /// preprocessing necessary for it to return the vector of values in an
     /// efficient manner.
     ///
-    /// This must be called before rendering begins and before the samples are
-    /// actually requested using `get_2d_array`.
-    fn prepare_2d_array(&mut self, sample_count: usize);
+    /// This must be called before rendering begins for the current pixel and
+    /// before the values are actually requested using `get_2d_array`.
+    fn prepare_2d_array(&mut self, count: usize);
 
-    /// When generating arrays of samples, a sampler might be more efficient at
-    /// generating sample arrays of certain lengths. This method takes an ideal
-    /// sample count and returns a sample count that is close and that the
-    /// sampler can generate efficiently.
-    fn round_count(&self, sample_count: usize) -> usize {
-        sample_count
+    /// When generating a vector of values for a sample vector, a sampler might
+    /// be more efficient at generating value vectors of certain lengths. This
+    /// method takes an ideal value count and returns a value count that is
+    /// close and that the sampler can generate efficiently.
+    fn round_count(&self, ideal_count: usize) -> usize {
+        ideal_count
     }
 
-    /// Get a vector 1D samples for the next dimension of the sample vector.
-    /// This method mutates the sampler by incrementing the current sample
-    /// dimension by one.
+    /// Get a vector of 1D values for the next dimension of the current sample
+    /// vector. This method mutates the sampler by incrementing the current
+    /// sample dimension by one.
     ///
     /// This must be called after `prepare_1d_array`. The returned vector will
-    /// contain the number of samples that is specified in the call to
+    /// contain the number of values that is specified in the call to
     /// `prepare_1d_array`.
-    fn get_1d_array(&mut self);
+    fn get_1d_vec(&mut self) -> Option<&Vec<f32>>;
 
-    /// Get a vector 2D samples for the next dimension of the sample vector.
-    /// This method mutates the sampler by incrementing the current sample
-    /// dimension by two.
+    /// Get a vector of 2D values for the next two dimensions of the current
+    /// sample vector. This method mutates the sampler by incrementing the
+    /// current sample dimension by two.
     ///
     /// This must be called after `prepare_2d_array`. The returned vector will
-    /// contain the number of samples that is specified in the call to
+    /// contain the number of values that is specified in the call to
     /// `prepare_2d_array`.
-    fn get_2d_array(&mut self);
+    fn get_2d_vec(&mut self) -> Option<&Vec<Point2<f32>>>;
 
     /// Tell the sampler to start working on the next sample for the current
     /// pixel. This method mutates the sampler by updating the current sample
---
+++ src/sampler/state.rs
@@ -0,0 +1,117 @@
+use cgmath::Point2;
+
+/// Provides state management and book-keeping that is useful in the
+/// implementation of samplers.
+pub struct InnerSamplerState {
+    samples_per_pixel: usize,
+    current_pixel: Point2<usize>,
+    current_pixel_sample_index: usize,
+
+    /// Pre-computed 1D values for each sample in the pixel and for each 1D
+    /// dimension request.
+    ///
+    /// The outer vector contains an element for each vector request. Each of
+    /// these elements corresponds to a distinct dimension.
+    ///
+    /// The next vector contains an element for each sample in a pixel.
+    ///
+    /// The inner vector contains the requested number of 1D values for each
+    /// sample.
+    vecs_1d: Vec<Vec<Vec<f32>>>,
+
+    /// Pre-computed 2D values for each sample in the pixel and for each 1D
+    /// dimension request.
+    ///
+    /// The outer vector contains an element for each vector request. Each of
+    /// these elements corresponds to a distinct pair of adjacent dimensions.
+    ///
+    /// The next vector contains an element for each sample in a pixel.
+    ///
+    /// The inner vector contains the requested number of 2D values for each
+    /// sample.
+    vecs_2d: Vec<Vec<Vec<Point2<f32>>>>,
+
+    /// The current index into `vecs_1d`. This is equivalent to the number of
+    /// requests to `get_1d_vec` for the current sample.
+    vecs_1d_current_index: usize,
+
+    /// The current index into `vecs_2d`. This is equivalent to the number of
+    /// requests to `get_2d_vec` for the current sample.
+    vecs_2d_current_index: usize,
+}
+
+impl InnerSamplerState {
+    fn new(samples_per_pixel: usize) -> Self {
+        Self {
+            samples_per_pixel,
+            current_pixel: Point2::new(0, 0),
+            current_pixel_sample_index: 0,
+
+            vecs_1d: vec![],
+            vecs_2d: vec![],
+            vecs_1d_current_index: 0,
+            vecs_2d_current_index: 0,
+        }
+    }
+
+    fn samples_per_pixel(&self) -> usize {
+        self.samples_per_pixel
+    }
+
+    fn current_pixel(&self) -> Point2<usize> {
+        self.current_pixel
+    }
+
+    fn current_pixel_sample_index(&self) -> usize {
+        self.current_pixel_sample_index
+    }
+
+    fn reset_current_sample_vecs_indices(&mut self) {
+        self.vecs_1d_current_index = 0;
+        self.vecs_2d_current_index = 0;
+    }
+
+    fn start_pixel(&mut self, pixel: Point2<usize>) {
+        self.current_pixel = pixel;
+        self.current_pixel_sample_index = 0;
+        self.reset_current_sample_vecs_indices();
+    }
+
+    fn prepare_1d_array(&mut self, count: usize) {
+        // Initialize a `count`-length vec for each sample in the pixel.
+        self.vecs_1d
+            .push(vec![vec![0.0; count]; self.samples_per_pixel]);
+    }
+
+    fn prepare_2d_array(&mut self, count: usize) {
+        // Initialize a `count`-length vec for each sample in the pixel.
+        self.vecs_2d.push(vec![
+            vec![Point2::new(0.0, 0.0); count];
+            self.samples_per_pixel
+        ]);
+    }
+
+    fn get_1d_vec(&mut self) -> Option<&Vec<f32>> {
+        self.vecs_1d
+            .get(self.vecs_1d_current_index)
+            .and_then(|vec_per_sample| vec_per_sample.get(self.current_pixel_sample_index))
+    }
+
+    fn get_2d_vec(&mut self) -> Option<&Vec<Point2<f32>>> {
+        self.vecs_2d
+            .get(self.vecs_2d_current_index)
+            .and_then(|vec_per_sample| vec_per_sample.get(self.current_pixel_sample_index))
+    }
+
+    fn start_next_sample(&mut self) -> bool {
+        self.reset_current_sample_vecs_indices();
+        self.current_pixel_sample_index += 1;
+        self.current_pixel_sample_index < self.samples_per_pixel
+    }
+
+    fn start_nth_sample(&mut self, sample_index: usize) -> bool {
+        self.reset_current_sample_vecs_indices();
+        self.current_pixel_sample_index = sample_index;
+        self.current_pixel_sample_index < self.samples_per_pixel
+    }
+}
